8c0425573c8159765c3bd6634e85af56b6cff813
Vince <vhorvath2010@gmail.com>
2022-09-08 01:30:10 -0400

    Added web API

---
+++ deploy/app.py
@@ -0,0 +1,21 @@
+import pickle
+import flask
+import os
+
+app = flask.Flask('Stellar Classifier')
+PORT = int(os.getenv('PORT', 9099))
+
+rf = pickle.load(open('rf.pkl', 'rb'))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    features = flask.request.get_json(force=True)['features']
+    x = [features['temperature'], features['luminosity'], features['radius'], features['absolute magnitude']]
+    prediction = rf.predict([x])
+    types = ["Red Dwarf", "Brown Dwarf", "White Dwarf", "Main Sequence", "SuperGiants", "HyperGiants"]
+    response = {'prediction': types[prediction[0]]}
+
+    return flask.jsonify(response)
+
+app.run(port=PORT)
